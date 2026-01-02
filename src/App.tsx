5d3c31cc4a4359b6adefaf0348c373fafcfc2fb2
Bhavnoor Singh Saroya <bsaroya3@my.bcit.ca>
2026-01-02 05:01:16 -0800

    forgot nbsp

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ export default function App() {
         <Text className="text-muted-foreground text-center">
           <span className="text-sm">&copy; 2025 BCIT Computing Club</span>
           <br />
-          <span className="text-xs">made by a<a className="underline" href="https://saroya.dev">human</a> </span>
+          <span className="text-xs">made by a<a className="underline" href="https://saroya.dev">&nbsp human</a> </span>
         </Text>
       </footer>
     </div>
